b2d88184c3390f2127b92e80e1deb6a3b0fd036a
Jessica <jduquedeveloper@gmail.com>
2024-06-25 17:57:46 -0300

    Final build for jam

--- Assets/Game/Scripts/UI/EndUIManager.cs
+++ Assets/Game/Scripts/UI/EndUIManager.cs
@@ -4,23 +4,23 @@ using UnityEngine.UI;
 
 public class EndManager : MonoBehaviour
 {
-    [SerializeField] private Button b_playAgain;
+    //[SerializeField] private Button b_playAgain;
     [SerializeField] private Button b_exit;
-    [SerializeField] private Image _gradeImage;
-    [SerializeField] private Sprite[] _grades;
+    //[SerializeField] private Image _gradeImage;
+    //[SerializeField] private Sprite[] _grades;
 
     private LevelUIManager _levelUIManager => LevelUIManager.I;
     private void Start()
     {
         b_exit.onClick.AddListener(ControlEndPanel);
-        b_playAgain.onClick.AddListener(PlayAgain);
+        //b_playAgain.onClick.AddListener(PlayAgain);
     }
 
     private void OnEnable()
     {
         ButtonsActivationControl(false);
         StartCoroutine(EnableInputCooldowns(Helpers.blackFadeTime));
-        SetGradeImage();
+        //SetGradeImage();
     }
 
     private void OnDisable()
@@ -28,27 +28,27 @@ public class EndManager : MonoBehaviour
         StopAllCoroutines();
     }
 
-    private void SetGradeImage()
-    {
-        switch (GameController.I._finalScorePercent)
-        {
-            case < 20:
-                _gradeImage.sprite = _grades[4];
-                break;
-            case < 40:
-                _gradeImage.sprite = _grades[3];
-                break;
-            case < 70:
-                _gradeImage.sprite = _grades[2];
-                break;
-            case < 90:
-                _gradeImage.sprite = _grades[1];
-                break;
-            case >= 90:
-                _gradeImage.sprite = _grades[0];
-                break;
-        }
-    }
+    //private void SetGradeImage()
+    //{
+    //    switch (GameController.I._finalScorePercent)
+    //    {
+    //        case < 20:
+    //            _gradeImage.sprite = _grades[4];
+    //            break;
+    //        case < 40:
+    //            _gradeImage.sprite = _grades[3];
+    //            break;
+    //        case < 70:
+    //            _gradeImage.sprite = _grades[2];
+    //            break;
+    //        case < 90:
+    //            _gradeImage.sprite = _grades[1];
+    //            break;
+    //        case >= 90:
+    //            _gradeImage.sprite = _grades[0];
+    //            break;
+    //    }
+    //}
 
     private IEnumerator EnableInputCooldowns(float seconds)
     {
@@ -58,8 +58,8 @@ public class EndManager : MonoBehaviour
 
     public void ControlEndPanel()
     {
-        ButtonsActivationControl(false);
-        _levelUIManager.ControlEndPanel(false);
+        BlackScreenController.I.FadeOutScene("MainMenu");
+        AudioManager.I.PlayCrossFade("menumusic");
     }
 
     #region Buttons
